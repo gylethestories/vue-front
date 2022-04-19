98403a9c7661beee504093f87f489b01fb84259a
gylethestories <294104561@qq.com>
2022-04-19 18:46:00 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <ul>
+   <li v-for="(user) in users" :key="user.id">{{user.name}}</li>
+ </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import axios from 'axios'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+     axios.get('http://localhost:3000/api/users').then(response=> {
+       this.users = response.data;
+     })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
